4acabd8eb05bf5ae1e3859bbda7471d44b2754ee
Tony Fabeen <tony.fabeen@akatus.com>
2013-05-24 14:02:09 -0300

    Find if App is Active or not in a dictionary State

--- src/http_routing_mesh.erl
+++ src/http_routing_mesh.erl
@@ -1,6 +1,8 @@
 -module(http_routing_mesh).
 -export([start/0, init/3, handle/2, terminate/3]).
 
+-record(applications,{urls}).
+
 start() ->
   application:start(crypto),
   application:start(ranch),
@@ -15,15 +17,39 @@ start() ->
   ).
 
 init({tcp, http}, Req, _Opts) ->
-  {ok, Req, undefined}.
+  State = #applications{urls=setup_applications()},
+  {ok, Req, State}.
 
+%%
 handle(Req, State) ->
   {Host, Req1} = cowboy_req:host(Req),
-  Host1 = binary_to_list(Host),
-  Response = ["<h3> Requested Host : ", Host1, " </h3>"],
-  io:format("Generated Response ~p~n", [Response]),
-  {ok, Req2} = cowboy_req:reply(200, [], Response, Req1),
-  {ok, Req2, State}.
+  delegate_to_app(Host, Req1, State).
 
+%%
 terminate(_Reason, _Req, _State) ->
   ok.
+
+%% Find Application
+delegate_to_app(Host, Req, State) ->
+
+    case orddict:find(Host, State#applications.urls) of
+        {ok,AppStatus} ->
+            io:format("App is ~p~n", AppStatus);
+        error ->
+            io:format("App NOT FOUND ~n")
+    end,
+
+    Host1 = binary_to_list(Host),
+    Response = ["<h3> Requested Host : ", Host1, " </h3>"],
+    io:format("Generated Response ~p~n", [Response]),
+    {ok, Req2} = cowboy_req:reply(200, [], Response, Req),
+    {ok, Req2, State}.
+
+%% Just to Test
+setup_applications() ->
+    A1 = orddict:new(),
+    A2 = orddict:append(<<"localhost">>, active, A1),
+    A3 = orddict:append(<<"myhost">>, active, A2),
+    A4 = orddict:append(<<"hostinactive">>, inactive, A3),
+    A4.
+
