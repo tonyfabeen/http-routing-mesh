ee11e58900b29db5a3c544487f31c3178e85c410
Tony Fabeen <tony.fabeen@gmail.com>
2013-05-25 13:27:00 -0300

    Dump Incoming Request gitignore

--- src/http_proxy.erl
+++ src/http_proxy.erl
@@ -20,9 +20,37 @@ init({tcp, http}, Req, _Opts) ->
   {ok, Req, undefined}.
 
 handle(Req, State) ->
-    {Host, _ } = cowboy_req:host(Req),
-    {ok, Req2 } = cowboy_req:reply(200, [], ["OK, HTTP for: ", binary_to_list(Host)], Req),
+    {Host, _ }    = cowboy_req:host(Req),
+    request_dump(Req),
+    {ok, Req2 }   = cowboy_req:reply(200, [], ["OK, HTTP for: ", binary_to_list(Host)], Req),
     {ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
+
+%% Extract request Values
+request_dump(Req) ->
+  {Method, _}  = cowboy_req:method(Req),
+  {Host, _ }    = cowboy_req:host(Req),
+  {HostUrl, _}  = cowboy_req:host_url(Req),
+  {Port, _}     = cowboy_req:port(Req),
+  {Headers, _}  = cowboy_req:headers(Req),
+  {PathInfo, _} = cowboy_req:path_info(Req),
+  {Path, _}     = cowboy_req:path(Req),
+  {QueryString, _} = cowboy_req:qs(Req),
+  {QueryStringVals, _} = cowboy_req:qs_vals(Req),
+
+  io:format("METHOD : ~p ~n", [Method]),
+  io:format("HOST : ~p ~n", [Host]),
+  io:format("HOST URL : ~p ~n", [HostUrl]),
+  io:format("PORT : ~p ~n", [Port]),
+  io:format("HEADERS : ~p ~n", [Headers]),
+  io:format("PATH INFO : ~p ~n", [PathInfo]),
+  io:format("PATH : ~p ~n", [Path]),
+  io:format("QUERY_STRING : ~p ~n", [QueryString]),
+  io:format("QUERY_STRING VALUES : ~p ~n", [QueryStringVals]).
+
+
+
+
