1ff162d2706df31bfc0f5c4b0759c143354e3bbd
Tony Fabeen <tony.fabeen@akatus.com>
2013-05-29 09:58:44 -0300

    Erlang WebSocket handler

---
+++ src/websockets_routing_mesh.erl
@@ -0,0 +1,22 @@
+-module(websockets_routing_mesh).
+-behaviour(cowboy_websocket_handler).
+-export([init/3, websocket_init/3, websocket_handle/3, websocket_info/3, websocket_terminate/3]).
+
+init({tcp, http}, _Req, _Opts) ->
+    {upgrade, protocol, cowboy_socket}.
+
+%% Behaviour Callbacks
+websocket_init(_TransportName, Req, _Opts) ->
+    {ok, Req, undefined_state}.
+
+websocket_handle({text, Msg}, Req, State) ->
+    io:format("Message to ECHO : ~p ~n", [Msg]),
+    {reply, {text, <<"ECHO : ",Msg/binary>>}, Req, State};
+websocket_handle(_Data, Req, State) ->
+    {ok, Req, State}.
+
+websocket_info(_Info, Req, State) ->
+    {ok, Req, State}.
+
+websocket_terminate(_Reason, _Req, _State) ->
+    ok.
