2d183c103677338719fa5b32bdf86c9556403473
Tony Fabeen <tony.fabeen@gmail.com>
2013-06-01 12:16:37 -0300

    Log Lines Removed

--- src/http_routing_mesh.erl
+++ src/http_routing_mesh.erl
@@ -9,7 +9,9 @@ start() ->
   application:start(cowboy),
   application:start(http_routing_mesh).
 
-%%
+%% =======================================
+%% Cowboy Callbacks
+%% =======================================
 init({tcp, http}, Req, _Opts) ->
   {ok, Client} = cowboy_client:init([]),
   {ok, ERedis}  = eredis:start_link(),
@@ -18,47 +20,63 @@ init({tcp, http}, Req, _Opts) ->
 
 %%
 handle(Req, State) ->
+
   {Host, Req1} = cowboy_req:host(Req),
+
   case find_app_for(Host, State) of
-    [AppUrl] ->
+
+    {AppInstances, AppUrl} when AppInstances >= 1 ->
         delegate_request_to_app(Req1, AppUrl, State);
-    [] ->
-        io:format("App NOT FOUND ~n"),
+    _ ->
         {ok, Req2 }   = cowboy_req:reply(404, [], "<h1>Sorry, App Not Found</h1>", Req1),
         {ok, Req2, State}
+
   end.
 
 %%
 terminate(_Reason, _Req, _State) ->
   ok.
 
+
+%% =======================================
+%% Private Functions
+%% =======================================
+
 %% Find Application
 find_app_for(Host,State) ->
-    {ok, AppUrl} = eredis:q(State#state.eredis, ["LRANGE", Host, "0", "0"]),
-    AppUrl.
+  AppInstances = app_instances_for(Host, State),
+  AppUrl = app_url_for(AppInstances, Host, State),
+  {AppInstances, AppUrl}.
+
+%%Return Nr of Instances of an APP
+app_instances_for(Host, State) -> 
+  {_, NrInstances} = eredis:q(State#state.eredis, ["LLEN", Host]),
+  AppInstances = list_to_integer(binary_to_list(NrInstances)),
+  AppInstances.
+
+%% 
+app_url_for(0, _Host, _State) -> <<"">>;
+app_url_for(AppInstances, Host, State) ->
+  {_, AppUrls} = eredis:q(State#state.eredis, ["LRANGE", <<"http:",Host/binary>>, "0", "-1"]),
+  AppUrl = lists:nth(random:uniform(AppInstances), AppUrls),
+  AppUrl.
 
 
 %% Delegate a Request to an application
 delegate_request_to_app(Req, AppUrl, State) ->
 
-    io:format("App is registered for => URL :  ~p ~n", [AppUrl]),
+  %% Send Request to App
+  {Method, RequestUrl, Headers} = request_dump(AppUrl, Req),
+  {ok, Client2} = cowboy_client:request(Method, RequestUrl, Headers, State#state.http_client),
+  {ok, ResponseStatus, ResponseHeaders, Client3} = cowboy_client:response(Client2),
+  {ok, ResponseBody, _Client4} = cowboy_client:stream_body(Client3),
 
-    %% Send Request to App
-    {Method, RequestUrl, Headers} = request_dump(AppUrl, Req),
-    {ok, Client2} = cowboy_client:request(Method, RequestUrl, Headers, State#state.http_client),
-    {ok, ResponseStatus, ResponseHeaders, Client3} = cowboy_client:response(Client2),
-    {ok, ResponseBody, _Client4} = cowboy_client:stream_body(Client3),
-
-    %% Send Response
-    io:format("ResponseStatus ~p ~n", [ResponseStatus]),
-    io:format("ResponseHeaders ~p ~n", [ResponseHeaders]),
-    io:format("ResponseBody ~p ~n", [ResponseBody]),
-
-    {ok, Req2 }   = cowboy_req:reply(ResponseStatus, ResponseHeaders, ResponseBody, Req),
-    {ok, Req2, State}.
+  %% Send Response Client
+  {ok, Req2 }   = cowboy_req:reply(ResponseStatus, ResponseHeaders, ResponseBody, Req),
+  {ok, Req2, State}.
 
 
-%% Extract request Values
+%% Extract request Values and Build Request Url to Send to a Backend
 request_dump(AppUrl, Req) ->
   {Method, _}  = cowboy_req:method(Req),
   {Host, _ }    = cowboy_req:host(Req),
@@ -69,16 +87,6 @@ request_dump(AppUrl, Req) ->
   {Path, _}     = cowboy_req:path(Req),
   {QueryString, _} = cowboy_req:qs(Req),
   {QueryStringVals, _} = cowboy_req:qs_vals(Req),
-
-  io:format("METHOD : ~p ~n", [Method]),
-  io:format("HOST : ~p ~n", [Host]),
-  io:format("HOST URL : ~p ~n", [HostUrl]),
-  io:format("PORT : ~p ~n", [Port]),
-  io:format("HEADERS : ~p ~n", [Headers]),
-  io:format("PATH INFO : ~p ~n", [PathInfo]),
-  io:format("PATH : ~p ~n", [Path]),
-  io:format("QUERY_STRING : ~p ~n", [QueryString]),
-  io:format("QUERY_STRING VALUES : ~p ~n", [QueryStringVals]),
 
   RequestUrl = <<AppUrl/binary, Path/binary, QueryString/binary>>,
   {Method, RequestUrl, Headers}.
